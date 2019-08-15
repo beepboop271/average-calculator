43f682bac0ef1224e4f3f0f54cd3d244c990b1be
beepboop271 <>
2019-08-15 00:51:01 -0400

    develop gui
    window scrolling, started forming main page layout

--- average calculator.py
+++ average calculator.py
@@ -1,27 +1,34 @@
 import pygame
 from classes.file_reader import unpack_file
-from drawing.screen_elements import Window
+from drawing.screen_elements import Window, Button
 
 pygame.init()
 
-# output precision
 STRAND_PRECISION = 5
 AVERAGE_PRECISION = 10
 
+BACKGROUND_CLR = (18, 18, 18)
+ELEMENT_CLR = (49, 49, 49)
+TEXT_CLR = (144, 144, 144)
+
 clock = pygame.time.Clock()
+MOUSE_SCROLL_UP = 4
+MOUSE_SCROLL_DOWN = 5
+MOUSE_SCROLL_AMOUNT = 12
 width = 1000
 height = 600
 display = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("asdfghjkl")
 
 courses = unpack_file("example_text_data.txt")
-
-current_window = Window("main", width, height, (0, 0, 0))
+current_window = Window("main", width, 200+(len(courses)*150), BACKGROUND_CLR, display)
+for i in range(len(courses)):
+    current_window.elements.append(Button(courses[i].course, width/2, 200+(150*i + 75), 700, 100, ELEMENT_CLR, courses[i].course))
 
 
 def drawWindow(window):
     window.draw()
-    display.blit(window.surface, (0, 0))
+    display.blit(window.surface, (0, -window.visible_top))
 
 
 running = True
@@ -29,6 +36,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == MOUSE_SCROLL_UP:
+                current_window.scroll(-MOUSE_SCROLL_AMOUNT)
+            elif event.button == MOUSE_SCROLL_DOWN:
+                current_window.scroll(MOUSE_SCROLL_AMOUNT)
 
     drawWindow(current_window)
     pygame.display.flip()
