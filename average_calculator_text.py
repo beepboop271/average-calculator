dc5158d40f2eac87bf443bea80a347f83bb1e244
beepboop271 <>
2019-09-16 21:45:03 -0400

    basic teachassist access
    grab html of course reports, extracting useful info to come

--- average_calculator_text.py
+++ average_calculator_text.py
@@ -5,6 +5,17 @@ from classes.teachassist_scraper import get_from_ta
 STRAND_PRECISION = 5
 AVERAGE_PRECISION = 10
 
+with open("auth.txt") as f:
+    TA_INFO = f.read().split("\n")
+
+AUTH_DICT = {"username": TA_INFO[0],
+             "password": TA_INFO[1]}
+TA_STUDENT_ID = TA_INFO[2]
+TA_SUBJECT_IDS = TA_INFO[3:]
+
+# program currently prints html of course report pages,
+# extracting useful data to come
+get_from_ta(AUTH_DICT, TA_STUDENT_ID, TA_SUBJECT_IDS)
 courses = unpack_file("example_text_data.txt")
 for course in courses:
     course.calculate_course_mark()
--- classes/teachassist_scraper.py
+++ classes/teachassist_scraper.py
@@ -1,5 +1,18 @@
 import requests
+TA_LOGIN_URL = "https://ta.yrdsb.ca/yrdsb/"
+TA_COURSE_BASE_URL = "https://ta.yrdsb.ca/live/students/viewReport.php"
 
 
-def get_from_ta():
-    pass
+def get_from_ta(auth_dict, student_id, subject_ids):
+    ta_session = requests.session()
+    ta_session.post(TA_LOGIN_URL, auth_dict)
+    for subject_id in subject_ids:
+        print ta_session.get(get_httpGET_url(student_id, subject_id)).content
+
+
+def get_httpGET_url(student_id, subject_id):
+    return (TA_COURSE_BASE_URL
+            + "?subject_id="
+            + str(subject_id)
+            + "&student_id="
+            + str(student_id))
