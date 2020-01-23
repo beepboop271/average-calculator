81d8ba8fb9d46408eab2d6e5ff75c312e08d47a9
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2020-01-22 22:09:34 -0500

    copying firebase guides lmao

--- android/AverageCalculator/app/src/main/java/com/example/averagecalculator/MainActivity.kt
+++ android/AverageCalculator/app/src/main/java/com/example/averagecalculator/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.averagecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        val t: TextView = findViewById(R.id.shoot_me);
+        t.setText(R.string.textTest2);
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("MainActivity", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d("MainActivity", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
     }
 }
---
+++ android/AverageCalculator/app/src/main/java/com/example/averagecalculator/MyFirebaseMessagingService.kt
@@ -0,0 +1,20 @@
+package com.example.averagecalculator
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d("MyFirebaseMsgingService", "Refreshed token: $token");
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+        Log.d("MyFirebaseMsgingService", "sendRegistrationTokenToServer($token)")
+    }
+}
